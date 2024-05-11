cc93a2ec68eccec863a69cd53dc7fc4a48bc3020
Lenovo <Lenovo@LAPTOP-12UO4LG1>
2024-05-11 10:38:24 +0700

    class stack : Public (int)

--- Stack Using Linked List/Stack Using Linked List.cpp
+++ Stack Using Linked List/Stack Using Linked List.cpp
@@ -21,5 +21,12 @@ public:
 		top = NULL;
 	}
 
-	
+	int push(int value) {
+		Node* newNode = new Node();
+		newNode->data = value;
+		newNode->next = top;
+		top = newNode;
+		cout << "Push Value : " << value << endl;
+		return value;
+	}
 };
